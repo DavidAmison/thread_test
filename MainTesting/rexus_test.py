551f349930f3b2922849e9959adcaebfb9b71542
David <david.amison20@gmail.com>
2017-03-17 17:26:10 +0000

    Timing
    Added a timer

--- MainTesting/rexus_test.py
+++ MainTesting/rexus_test.py
@@ -6,5 +6,9 @@ Created on Fri Mar 17 16:49:16 2017
 """
 
 import IMU
+import timeit
 
+start = timeit.default_timer()
 IMU.collect_date(10)
+length = timeit.default_timer() - start
+print('collecting data took:',length,'s')
